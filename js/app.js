20b77448540ab2a3b838bf52d2eca5e2804d2fb2
Nikita <nsotnikov16@yandex.ru>
2022-07-15 16:56:17 +0500

    update

--- js/app.js
+++ js/app.js
@@ -16,3 +16,18 @@ var swiperExamples = new Swiper('.examples .swiper', {
         prevEl: '.examples .swiper .swiper-button-prev',
     },
 })
+
+
+document.addEventListener('DOMContentLoaded', function (event) {
+    console.log(navigator.userAgent);
+    if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
+      var blocks = document.querySelectorAll('.mobile_bottom__nav ul li');
+      if (!blocks) return;
+      for (var block of blocks) {
+          block.style.height = 'auto';
+          block.style.paddingBottom = '0.8em';
+          block.style.paddingTop = '0.4em';
+      }
+    }
+});
+
